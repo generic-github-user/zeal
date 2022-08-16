2119500db3535da283b07914f9bd9a53a472377e
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-08-16 10:57:34 -0400

    add convenience class for key/value pairs

--- parser.py
+++ parser.py
@@ -34,8 +34,12 @@ class Node:
         self.type = self.source.data
         self.depth = depth
         for c in self.source.children:
-            if isinstance(c, lark.Tree): self.children.append(
-                    Node(c, self, self.depth+1, root=self.root if self.root else self))
+            if isinstance(c, lark.Tree):
+                subclass = Node
+                match c.data:
+                    case 'pair': subclass = Pair
+                self.children.append(subclass(
+                    c, self, self.depth+1, root=self.root if self.root else self))
             elif c is None: self.children.append(c)
             else:
                 assert isinstance(c, lark.Token), c
@@ -45,7 +49,12 @@ class Node:
         return ''.join(c.text() for c in self.children)
 
     def __str__(self):
-        return f'Node <{self.type}> ({self.depth})' + '\n' + '\n'.join('  '*self.depth + str(n) for n in self.children)
+        return f'{type(self).__name__} <{self.type}> ({self.depth})' + '\n' + '\n'.join('  '*self.depth + str(n) for n in self.children)
+
+class Pair(Node):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.key, self.value = self.children
 
 with open('grammar.lark', 'r') as grammar:
     parser = Lark(grammar.read(), parser='lalr', lexer='contextual', postlex=TreeIndenter(), debug=True)
